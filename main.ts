0bbefddc871609330b38771f4415a70736d287c3
kembriie <83826913+kembriie@users.noreply.github.com>
2021-05-10 08:38:16 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,37 @@
+namespace SpriteKind {
+    export const Key = SpriteKind.create()
+}
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    tiles.setWallAt(tiles.getTileLocation(13, 2), true)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . 3 3 . . . . . . . 
+        . . . . . . . 3 . 3 . . . . . . 
+        . . . . . . 3 . . 3 . . . . . . 
+        . . . . . . 3 . . 3 . . . . . . 
+        . . . . . . 3 . . 3 . . . . . . 
+        . . . . . . 3 . . 3 . . . . . . 
+        . . . . . . 3 . . 3 . . . . . . 
+        . . . . . 3 3 3 3 3 3 . . . . . 
+        . . . . . 3 3 3 3 3 3 3 . . . . 
+        . . . . . . 3 . . . . . . . . . 
+        . . . . . . 3 3 3 3 3 . . . . . 
+        . . . . . . 3 . . . 3 3 . . . . 
+        . . . . . . . . . . . 3 . . . . 
+        . . . . . . . . . . . 3 . . . . 
+        . . . . . . . . . . 3 3 . . . . 
+        . . . . . . . . . . 3 . . . . . 
+        `, mySprite, -100, 50)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
 	
 })
+let myEnemy: Sprite = null
+let projectile: Sprite = null
+let mySprite: Sprite = null
 tiles.setTilemap(tilemap`level1`)
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     . . . . . . . f f f f . . . . . 
     . . . . . f f f . . f . . . . . 
     . . . . . f . f . f f f . . . . 
@@ -22,3 +51,24 @@ let mySprite = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 100)
 scene.cameraFollowSprite(mySprite)
+game.onUpdateInterval(5000, function () {
+    myEnemy = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . 7 7 7 7 7 7 7 7 7 . . . . 
+        . . 7 7 . . . . . . . 7 7 7 . . 
+        . . 7 . . . . . . . . . . 7 7 . 
+        . 7 7 . . 7 7 . . 7 7 . . . 7 . 
+        7 7 . . . 7 7 . . 7 7 . . . 7 . 
+        7 . . . . . . . . . . . . . 7 . 
+        7 7 . . . 7 . . . . 7 7 . . 7 . 
+        . 7 . . . 7 7 7 7 7 7 . . . 7 . 
+        . . 7 . . . . . . . . . . 7 7 . 
+        . . 7 7 7 . . . 7 7 7 7 7 7 . . 
+        . . . . 7 7 7 7 . . . 7 7 . . . 
+        . . . . 7 7 . . . . . 7 7 . . . 
+        . . . . 7 7 . . . . . 7 7 . . . 
+        . . . . 7 7 . . . . . 7 7 . . . 
+        . . . 7 7 7 7 . . . 7 7 7 7 . . 
+        `, SpriteKind.Enemy)
+    myEnemy.follow(mySprite, 50)
+})
